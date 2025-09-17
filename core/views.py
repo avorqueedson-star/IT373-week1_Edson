200fae597d2144992b05ebf06b8bc117ac807366
avorqueedson-star <avorqueedson@gmail.com>
2025-09-18 00:33:07 +0800

    Updated templates and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def base(request):
+  return render(request, 'base.html')
 # Create your views here.
